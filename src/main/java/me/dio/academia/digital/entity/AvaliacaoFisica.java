5d189b763025a22cd8196c3fcdcb1c8c8c1e2c6b
lvantuire <lvantuire@gmail.com>
2022-06-03 14:46:15 -0300

    Subindo Evolução da Aula

--- src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
+++ src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
@@ -1,9 +1,21 @@
 package me.dio.academia.digital.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_avaliacoes")
 public class AvaliacaoFisica {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private Aluno aluno;
